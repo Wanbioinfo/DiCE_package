4c13099a13ec0b876c637d793a48deb3c8c24d56
Sandali Lokuge <sdlokuge@iu.edu>
2026-06-24 11:42:28 -0400

    Update Phase_3.R

--- R/Phase_3.R
+++ R/Phase_3.R
@@ -13,10 +13,10 @@
 #' @return Correlation matrix of the Phase 2 genes
 #' @noRd
 calculate_correlation <- function(class_geneExp, corr_method, corr_mode, corr_pval_cutoff){
-  if(corr_mode == "directCorr"){
+  if(tolower(corr_mode) == "directCorr"){
     corr_matrix <- traditional_corr(class_geneExp, corr_method, corr_pval_cutoff)
     
-  }else if (corr_mode == "remove_zerocells"){
+  }else if (tolower(corr_mode) == "remove_zerocells"){
     corr_matrix <- remove_Zerocells_corr(class_geneExp, corr_method, corr_pval_cutoff)
     
   }else if (tolower(corr_mode) %in% c("zinbwave", "newwave")){
